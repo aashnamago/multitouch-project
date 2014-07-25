1fb0aa85e50abb42bb25bc3c15b2fcb0a9f670a6
aashnamago <amago@stanford.edu>
2014-07-25 12:15:44 -0700

    updaaate

--- Assets/MultiTouchMenu.cs
+++ Assets/MultiTouchMenu.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 
 public class MultiTouchMenu : MonoBehaviour {
+	public struct fingerPair { // stores pair of fingerIds
+		public float finger1;
+		public float finger2;
+	}
 
 	private Dictionary<GameObject, List<float>> beingTouched = new Dictionary<GameObject, List<float>>(); // maps a GameObject that is being touched to a list of fingerIds associated with it
 	private Dictionary<float, Vector3> offsets = new Dictionary<float, Vector3>(); // maps a fingerId to its offset from the object it is associated with
 	private List<GameObject> alreadyExpanded = new List<GameObject>(); // records each GameObject that has already been expanded to display its submenus
 	private Dictionary<float, TouchPhase> touchPhases = new Dictionary<float, TouchPhase>(); // maps each existing fingerId to its current touch phase -- updated every frame
+	private Dictionary<fingerPair, float> fingerDistances = new Dictionary<fingerPair, float>(); // maps a finger pair to the distance between them
 
 	private const float originalY = 0f; // all objects should have the same original y-coordinate
 	private const float yScale = 0.0005f; // all objects should have the same y-scale
+	private const int numSubmenus = 6;
 
 	void Update() {
 		updateDictionary();
@@ -79,8 +85,46 @@ public class MultiTouchMenu : MonoBehaviour {
 					stationary++;
 				}
 			}
-
+			int numTouches = calculateTouches (button);
 			// START GOING THROUGH CASES:
+
+			// WHENEVER NUMTOUCHES = 2, RECALCULATE THE DISTANCE BETWEEN THOSE TWO AND STORE WITH GAMEOBJECT
+//			if (numTouches == 2) {
+//				fingerPair pair = new fingerPair();
+//				pair.finger1 = 0f;
+//				pair.finger2 = 0f;
+//				Vector3 position1 = new Vector3();
+//				Vector3 position2 = new Vector3();
+//				foreach (Touch currentTouch in InputProxy.touches) {
+//					if (beingTouched[button].Contains (currentTouch.fingerId)) {
+//						int layerToHit = 1 << 8; // only want to touch objects that are put in this layer
+//						Ray touchRay = camera.ScreenPointToRay(currentTouch.position);
+//						Debug.DrawRay(touchRay.origin, touchRay.direction * 200, Color.yellow); // figure out what camera.ScreenPointToRay does
+//						RaycastHit hitCheck;
+//						bool hitTrue = Physics.Raycast(touchRay, out hitCheck, 60f, layerToHit);
+//						GameObject hitObject = null;
+//						if (hitTrue) {
+//							hitObject = hitCheck.transform.gameObject;
+//							if (hitObject == button) {
+//								if (pair.finger1 == 0f) {
+//									pair.finger1 = currentTouch.fingerId;
+//									position1 = currentTouch.position;
+//								} else {
+//									pair.finger2 = currentTouch.fingerId;
+//									position2 = currentTouch.position;
+//								}
+//							}
+//						}
+//					}
+//				}
+//				Vector3 difference = position2 - position1;
+//				float distance = Vector3.magnitude(difference);
+//				if (!fingerDistances.ContainsKey(pair)){
+//					fingerDistances.Add(pair, distance);
+//				} else {
+//				}
+//
+//			} 
 			if (beingTouched[button].Count == 1 && moved == 1) {
 				foreach (Touch currentTouch in InputProxy.touches) {
 					if (currentTouch.fingerId == beingTouched[button][0]) {
@@ -88,43 +132,66 @@ public class MultiTouchMenu : MonoBehaviour {
 						Ray touchRay = camera.ScreenPointToRay(currentTouch.position);
 						Vector3 touchPosition = touchedPosition(touchRay, camera.transform, distanceFromCamera);
 						button.transform.position = touchPosition + offsets[currentTouch.fingerId]; 
-					} else {
-						Debug.Log("different finger"); // HAVING ISSUES HERE
 					}
 				}
-			}
-
-			if (beingTouched[button].Count == 3) {
-				int numTouches = calculateTouches (button);
-				if (numTouches == 1 && began == 0) {
+			} else if  (beingTouched[button].Count == 3) {
+				if (numTouches == 1 && began == 0) { // EXPAND
 					if (!alreadyExpanded.Contains (button)) {
 						generateButtons(button.transform);
 						alreadyExpanded.Add (button);
 					}
 				}
+				if (began == 1 && numTouches != 3) {
+					beingTouched[button].Clear ();
+					Debug.Log("happening");
+				}
 			}
+			if (beingTouched[button].Count > 1 && numTouches == 0) {
+				beingTouched[button].Clear ();
+			} 
 		}
 	}
 
 	void generateButtons(Transform hitObjectTransform) {
 		float cylinderScale = hitObjectTransform.localScale.x;
-		float radius = cylinderScale / 2.0f;
+		float newDiameter = 1.3f * cylinderScale;
 
-		// in the x direction
-		GameObject newButton = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-		newButton.name = "Submenu";
-		newButton.transform.localScale = new Vector3(cylinderScale / 2.5f, originalY, cylinderScale / 2.5f);
-		newButton.transform.position = hitObjectTransform.position + new Vector3(radius * 2, 0, 0);
-		newButton.renderer.material.color = Color.black;
-		newButton.layer = 8;
+		// update GUI labels positions
 
-		// in the z direction 
-		GameObject secondButton = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-		secondButton.name = "Submenu";
-		secondButton.transform.localScale = new Vector3(cylinderScale / 2.5f, originalY, cylinderScale / 2.5f);
-		secondButton.transform.position = hitObjectTransform.position + new Vector3(0, 0, radius * 2);
-		secondButton.renderer.material.color = Color.black;
-		secondButton.layer = 8;
+		// get angle and constant for number of submenus
+		float theta = (2 * Mathf.PI) / numSubmenus;
+
+		// general case 
+		for (int i = 1; i <= numSubmenus; i++) {
+			GameObject newButton = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+			newButton.name = "Submenu " + i;
+			newButton.transform.localScale = new Vector3(cylinderScale, originalY, cylinderScale);
+			newButton.transform.position = hitObjectTransform.position + new Vector3(newDiameter * Mathf.Cos(theta * i), 0 , newDiameter * Mathf.Sin(theta * i));
+			newButton.renderer.material.color = Color.black;
+			newButton.layer = 8;
+
+			GameObject buttonLabel = new GameObject ("label");
+			buttonLabel.AddComponent("GUIText");
+			buttonLabel.guiText.text = "Submenu";
+			Vector3 viewPos = camera.WorldToViewportPoint(newButton.transform.position);
+			buttonLabel.guiText.transform.position = viewPos;
+		}
+
+//		// in the x direction
+//		GameObject newButton = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+//		newButton.name = "Submenu";
+//
+//		newButton.transform.position = hitObjectTransform.position + new Vector3(radius * 2, 0, 0);
+//		newButton.renderer.material.color = Color.black;
+//		newButton.layer = 8;
+//
+//		// in the z direction 
+//		GameObject secondButton = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+//		secondButton.name = "Submenu";
+//		secondButton.transform.localScale = new Vector3(cylinderScale / 2.5f, originalY, cylinderScale / 2.5f);
+//		secondButton.transform.position = hitObjectTransform.position + new Vector3(0, 0, radius * 2);
+//		secondButton.renderer.material.color = Color.black;
+//		secondButton.layer = 8;
 	}
 
 	int calculateTouches(GameObject button) {
@@ -153,18 +220,37 @@ public class MultiTouchMenu : MonoBehaviour {
 		for (int j = 0; j < associatedFingers.Count; j++) {
 			float finger = associatedFingers[j];
 			Touch currentTouch = new Touch ();
+			bool stillExists = false;
 			foreach (Touch touch in InputProxy.touches) {
 				if (touch.fingerId == finger) {
-					currentTouch = touch;
+					stillExists = true;
 					break;
 				}
 			} 
-			if (currentTouch.phase == TouchPhase.Ended) {
+			if (!stillExists) {
 				beingTouched[button].Remove(finger); // update beingTouched dictionary
 				offsets.Remove(finger); // update offsets dictionary as well
 			}
 		}
 	}
+
+//	void deleteOldEntries(GameObject button) {
+//		List<float> associatedFingers = beingTouched[button];
+//		for (int j = 0; j < associatedFingers.Count; j++) {
+//			float finger = associatedFingers[j];
+//			Touch currentTouch = new Touch ();
+//			foreach (Touch touch in InputProxy.touches) {
+//				if (touch.fingerId == finger) {
+//					currentTouch = touch;
+//					break;
+//				}
+//			} 
+//			if (currentTouch.phase == TouchPhase.Ended) {
+//				beingTouched[button].Remove(finger); // update beingTouched dictionary
+//				offsets.Remove(finger); // update offsets dictionary as well
+//			}
+//		}
+//	}
 
 
 		 
